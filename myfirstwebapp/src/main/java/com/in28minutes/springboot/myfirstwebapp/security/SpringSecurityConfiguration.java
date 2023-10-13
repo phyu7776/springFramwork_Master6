5507c9452df897f6b872e2104a9e67c44b61f75b
ParkHwanYoung <phyu7776@douzone.com>
2023-10-13 17:28:17 +0900

    [SpringSecurity] 여러 사용자 추가

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -2,7 +2,6 @@ package com.in28minutes.springboot.myfirstwebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,15 +18,21 @@ public class SpringSecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager createInMemoryUserDetailsManager() {
 
+        UserDetails userDetails1 = createNewUser("phy", "123");
+        UserDetails userDetails2 = createNewUser("phy1", "123");
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    }
+
+    private UserDetails createNewUser (String username, String password) {
         Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
 
         UserDetails userDetails = User.builder()
                 .passwordEncoder(passwordEncoder)
-                .username("phy")
-                .password("123")
+                .username(username)
+                .password(password)
                 .roles("USER","ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(userDetails);
+        return userDetails;
     }
 
     @Bean
