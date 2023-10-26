c27737ce86b5bf1a4d7c23cab0f0ee482c126a61
ParkHwanYoung <phyu7776@douzone.com>
2023-10-26 13:59:19 +0900

    [H2DB] Bean을 사용한 기본 테이블 생성 data.sql 을 통한 기본 데이터 생성

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
@@ -1,10 +1,18 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity
 public class Todo {
+
+    public Todo() {
+    }
 
     public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
         this.id = id;
@@ -14,7 +22,10 @@ public class Todo {
         this.done = done;
     }
 
+    @Id
+    @GeneratedValue
     private int id;
+
     private String username;
     @Size(min=10, message = "Enter atleast 10 char")
     private String description;
---
+++ myfirstwebapp/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into todo (id, username, description, target_date, done ) values (1001, 'phy', 'Get AWS Certified', CURRENT_DATE ,false);
+insert into todo (id, username, description, target_date, done ) values (1002, 'phy', 'Get Azure Certified', CURRENT_DATE ,false);
+insert into todo (id, username, description, target_date, done ) values (1003, 'phy', 'Get GCP Certified', CURRENT_DATE ,false);
+insert into todo (id, username, description, target_date, done ) values (1004, 'phy', 'Get DevOps Certified', CURRENT_DATE ,false);
