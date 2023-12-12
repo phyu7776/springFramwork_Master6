e829143d087e74495372cb6f8877a90e1c77a2c9
박환용 <phyu7776@gmail.com>
2023-12-12 22:38:31 +0900

    jackson-dataformat-xml 을 통해 Accept의 타입에 따라 같은 api를 통하여도 다른 format 을 얻을수 있다
    i18n
    messages.properties 를 통한 다국어 처리
    messages 뒤에 _(언더바) 후 원하는 나라를 적고
    각 나라별 메세지 적은후
    messageSource 메소드를 통하여 Accept-Language
    따라 property 에 적힌 결과물을 받을수 있음

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HellowWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HellowWorldController.java
@@ -1,12 +1,22 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HellowWorldController {
+
+    private MessageSource messageSource;
+
+    public HellowWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     //@RequestMapping 에서는 Method를 입력해야 하지만 GetMapping, PostMapping  ... method 를 입력할 필요가없은 편리 ~~
     @GetMapping(path = "/hello-world")
@@ -22,5 +32,11 @@ public class HellowWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean("HelloWorld" + name);
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
