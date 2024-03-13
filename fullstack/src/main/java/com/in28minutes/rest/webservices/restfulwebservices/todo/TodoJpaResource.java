31773e12d0753074e50dbd60dc4045bbf53daf60
박환용 <phyu7776@gmail.com>
2024-03-13 23:15:13 +0900

    h2 database 를 이용한 TODO 기능 구현

--- fullstack/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoJpaResource.java
+++ fullstack/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoJpaResource.java
@@ -1,44 +1,53 @@
 package com.in28minutes.rest.webservices.restfulwebservices.todo;
 
+import com.in28minutes.rest.webservices.restfulwebservices.todo.repository.TodoRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//@RestController
-public class TodoResource {
+@RestController
+public class TodoJpaResource {
 
     private TodoService todoService;
 
-    public TodoResource(TodoService todoService){
+    private TodoRepository todoRepository;
+
+    public TodoJpaResource(TodoService todoService, TodoRepository todoRepository) {
         this.todoService = todoService;
+        this.todoRepository = todoRepository;
     }
 
     @GetMapping("/users/{username}/todos")
     public List<Todo> retrieveTodos(@PathVariable String username) {
-        return todoService.findByUsername(username);
+        return todoRepository.findByUsername(username);
     }
 
     @GetMapping("/users/{username}/todo/{id}")
     public Todo retrieveTodo(@PathVariable int id) {
-        return todoService.findById(id);
+        return todoRepository.findById(id).get();
     }
 
     @DeleteMapping("/users/{username}/todo/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id) {
-        todoService.deleteById(id);
+        todoRepository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/users/{username}/todo/{id}")
     public Todo updateTodo(@PathVariable String username, @PathVariable int id
             , @RequestBody Todo todo) {
-        todoService.updateTodo(todo);
+//        todoService.updateTodo(todo);
+        todoRepository.save(todo);
         return todo;
     }
 
     @PostMapping("/users/{username}/todo")
     public Todo createTodo(@PathVariable String username, @RequestBody Todo todo) {
-        return todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), todo.isDone());
+        todo.setUsername(username);
+        todo.setId(null);
+        return todoRepository.save(todo);
+
+//        return todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), todo.isDone());
     }
 }
--- fullstack/src/main/resources/data.sql
+++ fullstack/src/main/resources/data.sql
@@ -1,5 +1,4 @@
-insert into todo (id, username, description, target_date, done ) values (1001, 'phy', 'Get AWS Certified', CURRENT_DATE ,false);
-insert into todo (id, username, description, target_date, done ) values (1002, 'phy', 'Get Azure Certified', CURRENT_DATE ,false);
-insert into todo (id, username, description, target_date, done ) values (1003, 'phy', 'Get GCP Certified', CURRENT_DATE ,false);
-insert into todo (id, username, description, target_date, done ) values (1004, 'phy', 'Get DevOps Certified', CURRENT_DATE ,false);
-
+insert into todo (id, username, description, target_date, done ) values (1001, 'test', 'Get AWS Certified', CURRENT_DATE ,false);
+insert into todo (id, username, description, target_date, done ) values (1002, 'test', 'Get Azure Certified', CURRENT_DATE ,false);
+insert into todo (id, username, description, target_date, done ) values (1003, 'test', 'Get GCP Certified', CURRENT_DATE ,false);
+insert into todo (id, username, description, target_date, done ) values (1004, 'test', 'Get DevOps Certified', CURRENT_DATE ,false);
