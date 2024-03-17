8eae26c2aeddc8565d0915b6fd766eee8e02a10c
박환용 <phyu7776@gmail.com>
2024-03-17 23:28:24 +0900

    springSecurity init

---
+++ learn-spring-security/src/main/java/com/phy/leanspringsecurity/LeanSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.phy.leanspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeanSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ learn-spring-security/src/test/java/com/phy/leanspringsecurity/LeanSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phy.leanspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeanSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
